76dd8dd66f41790220555844420bc952a0c2f8b3
saucerman <gyq0929@gmail.com>
2022-11-13 23:14:19 +0800

    fix

--- list_func_from_class.js
+++ list_func_from_class.js
@@ -1,10 +1,10 @@
 function list_func_from_class(targetClass) {
     send("------------枚举类方法开始("+targetClass+")------------")
     var target = Java.use(targetClass);
-    var methods = hook.class.getDeclaredMethods();
-    hook.$dispose;
+    var methods = target.class.getDeclaredMethods();
+    target.$dispose;
     methods.forEach(function(method) {
-        send(method);
+        send(method.toString());
     });
     send("------------枚举类方法结束("+targetClass+")------------")
 }
